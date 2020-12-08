6211e73afb04817a36ee11582590e71698ea4115
ASUS <ASUS@DESKTOP-R0579L9>
2020-12-08 20:39:58 +0300

    Есть доработки

--- SOLID.cpp
+++ SOLID.cpp
@@ -9,11 +9,13 @@ int main()
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
-	//Rectangle rec(10, 20); - Не заработала 
+	Rectangle rec (20, 40);
+	Circle cir(30); 
+	Triangle tri(60, 70, 80); 
 
-	auto rec = new Rectangle(20, 40);
+	/*auto rec = new Rectangle(20, 40);
 	auto cir = new Circle(30);
-	auto tri = new Triangle(60, 70, 80);
+	auto tri = new Triangle(60, 70, 80);*/
 
 	cout << "Rectangle square: " << rec.getSquare() << endl;
 	cout << "Circle square: " << cir.getSquare() << endl;
